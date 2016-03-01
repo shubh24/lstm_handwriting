fe099afdf410970e6d715425947a5818e8eeb797
shubh24 <shubhankar24@gmail.com>
2016-03-01 11:27:03 +0530

    Convolutional Layers

--- softmax_90.py
+++ softmax_90.py
@@ -11,7 +11,6 @@ y_ = tf.placeholder(tf.float32, shape=[None, 10])
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 
-sess.run(tf.initialize_all_variables())
 
 y = tf.nn.softmax(tf.matmul(x,W) + b)
 
@@ -19,8 +18,10 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y))
 
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
+sess.run(tf.initialize_all_variables())
+
 for i in range(1000):
-	batch = mnist.train.next_batch(50)
+	batch = mnist.train.next_batch(100)
 	train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
